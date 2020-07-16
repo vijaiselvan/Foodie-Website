6f5eb079aac3ecae8d1475a918c6e310943b28f6
Vijai <vijayselvanvj@gmail.com>
2020-07-17 00:54:55 +0530

    added sticky navbar and smooth scrolling on navigation

---
+++ resources/js/script.js
@@ -0,0 +1,61 @@
+$(document).ready(function () {
+    $('.js--section-features').waypoint(
+        function (direction) {
+            if (direction == 'down') {
+                $('nav').addClass('sticky');
+            } else {
+                $('nav').removeClass('sticky');
+            }
+        },
+        {
+            offset: '60px',
+        }
+    );
+
+    // Scroll on buttons
+    $('.js--scroll-to-plans').click(function () {
+        $('html, body').animate(
+            { scrollTop: $('.js--section-plans').offset().top },
+            1000
+        );
+    });
+
+    $('.js--scroll-to-features').click(function () {
+        $('html, body').animate(
+            { scrollTop: $('.js--section-features').offset().top },
+            1000
+        );
+    });
+
+    $('a[href*="#"]')
+        .not('[href="#"]')
+        .not('[href="#0"]')
+        .click(function (event) {
+            if (
+                location.pathname.replace(/^\//, '') ==
+                    this.pathname.replace(/^\//, '') &&
+                location.hostname == this.hostname
+            ) {
+                var target = $(this.hash);
+                target = target.length
+                    ? target
+                    : $('[name=' + this.hash.slice(1) + ']');
+
+                if (target.length) {
+                    event.preventDefault();
+                    $('html, body').animate(
+                        {
+                            scrollTop: target.offset().top,
+                        },
+                        1000
+                    );
+                }
+            }
+        });
+
+    // var waypoints = $('#options-only').waypoint({
+    //     handler: function(direction) {
+    //       notify(this.element.id + ' hit')
+    //     }
+    //   })
+});
