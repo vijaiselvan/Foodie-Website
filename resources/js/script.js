9e69b10d5e578f86f52e48d0fdc1fc8159f5c887
Vijai Selvan <46491280+vijaiselvan@users.noreply.github.com>
2025-07-18 21:21:10 +0530

    Update script.js

--- resources/js/script.js
+++ resources/js/script.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
         }
     );
 
-    // Scroll on buttons
+    // Scroll on buttons 
     $('.js--scroll-to-plans').click(function () {
         $('html, body').animate(
             { scrollTop: $('.js--section-plans').offset().top },
